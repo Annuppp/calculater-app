2ffa8e380bd37e84f0224c08053f705dd16cd4c9
Annuppp <anupbhattachan10@gmail.com>
2024-07-04 13:33:31 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+
+const display = document.querySelector('.display');
+
+const AppendToDisplay = (input) => {
+
+  display.value += input;
+
+}
+
+const clearDisplay = () => {
+
+  display.value = "";
+
+}
+
+const calculate = () => {
+
+    try{
+         display.value = eval(display.value);
+    }
+   catch(error){
+      display.value = "Error";
+
+   }
+
+}
